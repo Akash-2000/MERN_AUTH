ff9f5766f60109f685f1262e30464d39243067aa
Vignesh1505-tech <itzvicky213@gmail.com>
2023-09-22 06:43:44 +0530

    new chnages

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -1,24 +1,74 @@
 import asyncHandler from "express-async-handler";
-
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 // @desc Auth user/set token
 //route POST/api/users/auth
 //@access Pubic
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const {email,password} = req.body
+
+  const user = await User.findOne({email})
+  if(user && (await user.matchPasswords(password))){
+    generateToken(res,user._id)
+    res.status(201).json({
+      _id:user._id,
+      name:user.name,
+      email:user.email
+    })
+   }else{
+    res.status(400);
+    throw new Error("Invalid email or password")
+   }
+
+
 });
 
 // @desc Register new user
 //route POST /api/users/register
 //@access Pubic
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register User" });
+  
+ const {name,email,password} = req.body;
+
+ const userExiisits = await User.findOne({email:email})
+
+ if(userExiisits){
+  res.status(400);
+  throw new Error('User already exists')
+ }
+  
+ const user = await User.create({
+  name,
+  email,
+  password
+ })
+
+ if(user){
+  generateToken(res,user._id)
+  res.status(201).json({
+    _id:user._id,
+    name:user.name,
+    email:user.email
+  })
+ }else{
+  res.status(400);
+  throw new Error("Invalide user data")
+ }
+  
 });
 
 // @desc logot user/set token
 //route POST/api/users/logout
 //@access Pubic
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+
+res.cookie('jwt','',{
+  httpOnly:true,
+  expires:new Date(0) 
+})
+
+res.status(200).json({message: 'User Logged out'})
+
 });
 
 // @desc Get user profile
--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,28 @@ import dotenv from "dotenv";
 dotenv.config();
 import { notFound, errorHandler } from "./middleware/errormiddleware.js";
 const port = process.env.PORT || 5000;
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 
+connectDB();
+
 const app = express();
+//to access the req.body values
+app.use(express.json());
+9//to acces the form data
+app.use(express.urlencoded({extended:true}))
 
-app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
+app.use(cookieParser())
+
+ app.use("/api/users", userRoutes);
+
+app.get("/hello", (req, res) => {
+  console.log("im ready")
   res.send("Server is ready");
 });
 
-app.use(notFound);
-app.use(errorHandler);
+ app.use(notFound);
+ app.use(errorHandler);
 app.listen(port, () => console.log("Server started on port" + port));
